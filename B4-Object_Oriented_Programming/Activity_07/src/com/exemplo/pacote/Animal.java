f216b69bc4852cf49ef445c824ff81bc8791cfff
Guilherme Conde <guilherme.mattos.conde@gmail.com>
2024-10-22 21:55:39 -0300

    Activity_07 OOP

---
+++ B4-Object_Oriented_Programming/Activity_07/src/com/exemplo/pacote/Animal.java
@@ -0,0 +1,13 @@
+package com.exemplo.pacote;
+
+public class Animal {
+    private String nome;
+
+    public void fazerSom() {
+        System.out.println("Som do animal");
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
