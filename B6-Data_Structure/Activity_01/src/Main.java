6f8c27e5c8b51dd23f0164dfc8a6db33a2414aef
Guilherme Mattos Conde <guilherme.mattos.conde@gmail.com>
2025-05-29 21:47:28 -0300

    atualização da main

--- B6-Data_Structure/Activity_01/src/Main.java
+++ B6-Data_Structure/Activity_01/src/Main.java
@@ -8,32 +8,38 @@ public class Main {
         arvore.getRaiz().getNoEsquerda().setNoDireita(new No("E"));
         arvore.getRaiz().getNoDireita().setNoDireita(new No("F"));
 
-        System.out.println("Quantidade de nós (recursiva): " + arvore.getQuantidadeNos(arvore.getRaiz()));
-        System.out.println("Quantidade de nós (não recursiva): " + arvore.getQuantidadeNosPilha());
-        System.out.println("Quantidade de nós (não recursiva): " + arvore.getQuantidadeNosFila());
+        System.out.println("*** Quantidade de nós ***");
+        System.out.println("Recursiva: " + arvore.getQuantidadeNos(arvore.getRaiz()));
+        System.out.println("Pilha (não recursiva): " + arvore.getQuantidadeNosPilha());
+        System.out.println("Fila (não recursiva): " + arvore.getQuantidadeNosFila());
 
-        System.out.println("Quantidade de folhas (recursiva): " + arvore.getQuantidadeFolhas(arvore.getRaiz()));
-        System.out.println("Quantidade de folhas (não recursiva): " + arvore.getQuantidadeFolhasNaoRecursiva());
+        System.out.println("\n*** Quantidade de folhas ***");
+        System.out.println("Recursiva: " + arvore.getQuantidadeFolhas(arvore.getRaiz()));
+        System.out.println("Não recursiva: " + arvore.getQuantidadeFolhasNaoRecursiva());
 
-        System.out.print("Árvore pré-ordem (recursiva): ");
+        System.out.println("\n*** Árvore pré-ordem ***");
+        System.out.print("Recursiva: ");
         arvore.preOrdem(arvore.getRaiz());
-
-        System.out.print("\nÁrvore pré-ordem (não recursiva): ");
+        System.out.print("\nNão recursiva: ");
         arvore.preOrdemNaoRecursiva();
 
-        System.out.print("\nÁrvore em-ordem (recursiva): ");
+        System.out.println();
+        System.out.println("\n*** Árvore em-ordem ***");
+        System.out.print("Recursiva: ");
         arvore.emOrdem(arvore.getRaiz());
-
-        System.out.print("\nÁrvore em-ordem (não recursiva): ");
+        System.out.print("\nNão recursiva: ");
         arvore.emOrdemNaoRecursiva();
 
-        System.out.print("\nÁrvore pós-ordem (recursiva): ");
+        System.out.println();
+        System.out.println("\n*** Árvore pós-ordem ***");
+        System.out.print("Recursiva: ");
         arvore.posOrdem(arvore.getRaiz());
-
-        System.out.print("\nÁrvore pós-ordem (não recursiva): ");
+        System.out.print("\nNão recursiva: ");
         arvore.posOrdemNaoRecursiva();
 
-        System.out.print("\nÁrvore em-nível: ");
+        System.out.println();
+        System.out.println("\n*** Árvore em-nível ***");
+        System.out.print("Não recursiva: ");
         arvore.emNivel();
     }
 }
