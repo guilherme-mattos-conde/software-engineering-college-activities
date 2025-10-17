95e603382ae84f35e85b7a495167cd6da6667366
Guilherme Mattos Conde <guilherme.mattos.conde@gmail.com>
2025-10-17 20:20:20 -0300

    activity-03

---
+++ 8th-bimester/rest-api/activity-01/src/test/java/com/github/guilherme_mattos_conde/activity_01/Activity01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.guilherme_mattos_conde.activity_01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Activity01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
